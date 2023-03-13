0ee8a5068ac3e63a5f448b27bb6c8ce86a6e1ec6
gongrunze <823650460@qq.com>
2023-03-13 23:44:29 +0800

    活动管理后端

--- rate/rateserver/rate-mapper/src/main/java/org/sys/rate/mapper/SecretaryMapper.java
+++ rate/rateserver/rate-mapper/src/main/java/org/sys/rate/mapper/SecretaryMapper.java
@@ -1,8 +1,33 @@
 package org.sys.rate.mapper;
 
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.sys.rate.model.Activities;
+import org.sys.rate.model.Experts;
+import org.sys.rate.model.Participates;
+
+import java.util.List;
 
 public interface SecretaryMapper {
     @Select("SELECT COUNT(*) from expertactivities WHERE teacherID = #{Id} AND is_secretary = 1 Limit 1")
     int isSecretary(Integer Id);
+
+    @Update("UPDATE expertactivities SET is_secretary = 1 WHERE teacherID = #{teacherID} AND activityID = #{activityID} AND groupID = #{groupID}")
+    int setSecretary(Integer teacherID, Integer activityID, Integer groupID);
+
+    @Select("SELECT a.ID,groupID,name,startDate,scoreItemCount,score,groupCount,expertCount,participantCount,`comment` \n" +
+            "FROM expertactivities e, activities a \n" +
+            "WHERE e.activityID = a.ID AND teacherID = #{teacherID} AND is_secretary = 1")
+    List<Activities> getAct(Integer teacherID);
+
+    @Select("SELECT s.ID,name,code\n" +
+            "FROM participants p, student s \n" +
+            "WHERE groupID = #{groupID} AND p.studentID = s.Id AND activityID = #{activityID}")
+    List<Participates> getPar(Integer activityID,Integer groupID);
+
+    @Select("SELECT t.ID,name,IDNumber\n" +
+            "FROM expertactivities e,teacher t \n" +
+            "WHERE groupID = #{groupID} AND e.teacherID = t.ID AND activityID = #{activityID}")
+    List<Experts> getEx(Integer activityID,Integer groupID);
 }
--- rate/rateserver/rate-web/src/main/java/org/sys/rate/controller/secretary/SecretaryController.java
+++ rate/rateserver/rate-web/src/main/java/org/sys/rate/controller/secretary/SecretaryController.java
@@ -1,10 +1,45 @@
 package org.sys.rate.controller.secretary;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.sys.rate.mapper.SecretaryMapper;
+import org.sys.rate.model.Activities;
+import org.sys.rate.model.Experts;
+import org.sys.rate.model.Participates;
+import org.sys.rate.model.RespBean;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/secretary")
 public class SecretaryController {
 
+    @Resource
+    SecretaryMapper secretaryMapper;
+
+    @GetMapping("/setSecretary")
+    @ResponseBody
+    public RespBean setSecretary(@RequestParam Integer teacherID,@RequestParam Integer activityID,@RequestParam Integer groupID){
+        int res = secretaryMapper.setSecretary(teacherID,activityID,groupID);
+        return res == 1 ? RespBean.ok("success",1) : RespBean.ok("fail",0);
+    }
+
+    @GetMapping("/getAct")
+    @ResponseBody
+    public RespBean getAct(@RequestParam Integer teacherID){
+        List<Activities> res= secretaryMapper.getAct(teacherID);
+        return RespBean.ok("success",res);
+    }
+
+    @GetMapping("/getMember")
+    @ResponseBody
+    public RespBean getMember(@RequestParam Integer activityID,@RequestParam Integer groupID){
+        List<Participates> participates = secretaryMapper.getPar(activityID,groupID);
+        List<Experts> experts = secretaryMapper.getEx(activityID,groupID);
+        List<Object> res = new ArrayList<>();
+        res.add(participates);
+        res.add(experts);
+        return RespBean.ok("success",res);
+    }
 }
