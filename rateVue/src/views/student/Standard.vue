1ab82eab77c07af410a12547f70ef33894d2124c
luyiru <luyiru2021@163.com>
2023-09-15 10:53:43 +0800

    1.调整标准选择指标点展开状态

--- rateVue/src/views/student/Standard.vue
+++ rateVue/src/views/student/Standard.vue
@@ -277,7 +277,8 @@
             @node-click="handleNodeClick"
             :expand-on-click-node="false"
             :highlight-current="true"
-            default-expand-all
+            node-key="id"
+            :default-expanded-keys="defaultExpandedKeys"
         ></el-tree>
       </span>
     </el-dialog>
@@ -290,6 +291,7 @@ export default {
   name: "SalSearch",
   data() {
     return {
+      defaultExpandedKeys: [],
       zeroPointReason: '',
       isAuthorIncludeSelf: false,
       indicatorBtn: '选择指标点',
@@ -391,8 +393,11 @@ export default {
     initTree() {
       this.getRequest("/indicator").then( resp => {
         this.showTreeDialog = true;
+        this.defaultExpandedKeys = [];
         if (resp) {
           this.indicatorData = resp.obj[1];
+          if(this.indicatorData.length > 0)
+            this.defaultExpandedKeys.push(this.indicatorData[0].id);
         }
       });
     },
