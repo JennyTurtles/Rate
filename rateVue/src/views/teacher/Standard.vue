fdaf1936df73d267b2e986eb24a4f6b77c4a6dfd
luyiru <luyiru2021@163.com>
2023-10-23 16:29:43 +0800

    1。标准调整预览功能

--- rateVue/src/views/teacher/Standard.vue
+++ rateVue/src/views/teacher/Standard.vue
@@ -298,14 +298,17 @@
           <span v-if="emp.url == '' || emp.url == null ? true:false" >无证明材料</span>
           <span v-else>{{ emp.url | fileNameFilter }}</span>
           <div>
+            <el-button @click="previewMethod('1')" v-show="isImage || isPdf">预览</el-button>
+            <el-button @click="previewMethod('2')">下载</el-button>
+          </div>
+          <div style="margin-top: 5px">
             <el-image
-                v-show="isImage"
+                v-show="false"
+                ref="previewImage"
                 style="width: 100px; height: 100px"
                 :src="previewUrl"
                 :preview-src-list="previewImageSrcList">
             </el-image>
-            <el-button @click="previewMethod('1')" v-show="!isImage">预览</el-button>
-            <el-button @click="previewMethod('2')">下载</el-button>
           </div>
           <br />
         </el-form-item>
@@ -359,12 +362,7 @@
           <el-button @click="isShowInfo = false">取消</el-button>
         </span>
     </el-dialog>
-    <el-dialog :visible.sync="dialogPreviewDocxFile" style="width: 100%;height: 100%">
-      <template v-if="isDocx">
-        <vue-office-docx
-            :src="previewUrl"
-            style="height: 100vh;"/>
-      </template>
+    <el-dialog :visible.sync="dialogPreviewPdfFile" style="width: 100%;height: 100%">
       <template v-if="isPdf">
         <vue-office-pdf
             :src="previewUrl"
@@ -384,9 +382,8 @@ export default {
   data() {
     return {
       isImage: false,
-      isDocx: false,
       isPdf: false,
-      dialogPreviewDocxFile: false,
+      dialogPreviewPdfFile: false,
       previewImageSrcList: [],
       previewUrl: '',
       pointBack: '',
@@ -480,8 +477,14 @@ export default {
       if(type == '1') {
         this.previewFileMethod(this.emp).then(res => {
           this.previewUrl = res;
+          if(this.isImage) {
+            this.previewImageSrcList = [res];
+            this.$refs.previewImage.showViewer = true;
+          }
+          if(this.isPdf) {
+            this.dialogPreviewPdfFile = true;
+          }
         });
-        this.dialogPreviewDocxFile = true;
       } else {
         this.downloadFileMethod(this.emp);
       }
@@ -623,13 +626,11 @@ export default {
       this.title_show = "显示详情";
       this.emp = data;
       this.dialogVisible_show = true;
-      this.isPdf = this.isImage = this.isDocx = false; //初始化
+      this.isPdf = this.isImage = false; //初始化
       this.previewUrl = '';
       this.previewImageSrcList = [];
       if(data.url.includes('.pdf')) { //判断文件类型
         this.isPdf = true;
-      } else if(data.url.includes('.docx')) {
-        this.isDocx = true;
       } else if(data.url.includes('.jpg') || data.url.includes('.png') || data.url.includes('.jpe')) {
         this.isImage = true;
       }
@@ -643,12 +644,6 @@ export default {
           })
         }
       });
-      if(this.isImage) {
-        this.previewFileMethod(data).then(res => {
-          this.previewUrl = res;
-          this.previewImageSrcList = [res];
-        });
-      }
     },
     //应该要分是否有无筛选条件
     sizeChange(currentSize) {
