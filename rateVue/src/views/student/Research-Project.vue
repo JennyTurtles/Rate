f788b5e5339d3ef282d2b6eb9ccea29af04cf08c
luyiru <luyiru2021@163.com>
2023-10-23 17:24:26 +0800

    1。学生纵向修改预览和下载功能

--- rateVue/src/views/student/Research-Project.vue
+++ rateVue/src/views/student/Research-Project.vue
@@ -287,11 +287,23 @@
         </el-form-item>
         <el-form-item label="证明材料:" prop="url">
           <span v-if="currentProject.url == '' || currentProject.url == null ? true:false" >无证明材料</span>
-          <a v-else style="color:gray;font-size:11px;text-decoration:none;cursor:pointer"
-             @click="download(currentProject)"
-             onmouseover="this.style.color = 'blue'"
-             onmouseleave="this.style.color = 'gray'">
-            {{currentProject.url|fileNameFilter}}</a>
+          <div v-else>{{ currentProject.url | fileNameFilter }}</div>
+          <br />
+          <div v-show="currentProject.url == '' || currentProject.url == null ? false : true">
+            <div>
+              <el-button @click="previewMethod('1')" v-show="isImage || isPdf">预览</el-button>
+              <el-button @click="previewMethod('2')">下载</el-button>
+            </div>
+            <div style="margin-top: 5px">
+              <el-image
+                  v-show="false"
+                  ref="previewImage"
+                  style="width: 100px; height: 100px"
+                  :src="previewUrl"
+                  :preview-src-list="previewImageSrcList">
+              </el-image>
+            </div>
+          </div>
           <br />
         </el-form-item>
         <el-form-item label="相关备注:">
@@ -316,7 +328,14 @@
         >
       </span>
     </el-dialog>
-
+    <el-dialog :visible.sync="dialogPreviewPdfFile" style="width: 100%;height: 100%">
+      <template v-if="isPdf">
+        <vue-office-pdf
+            :src="previewUrl"
+            style="height: 100vh;"
+        />
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -328,6 +347,11 @@ export default {
   name: "SalSearch",
   data() {
     return {
+      isImage: false,
+      isPdf: false,
+      dialogPreviewPdfFile: false,
+      previewImageSrcList: [],
+      previewUrl: '',
       currentIndicator: {},
       zeroPointReason: '',
       searchTypeLoading: false,
@@ -411,6 +435,23 @@ export default {
     }
   },
   methods: {
+    previewMethod(type) {
+      if(type == '1') {
+        this.previewFileMethod(this.currentProject).then(res => {
+          this.previewUrl = res;
+          if(this.isImage) {
+            this.previewImageSrcList = [res];
+            this.$refs.previewImage.showViewer = true;
+          }
+          if(this.isPdf) {
+            this.dialogPreviewPdfFile = true;
+          }
+        });
+      } else {
+        this.downloadFileMethod(this.currentProject);
+      }
+    },
+
     //选择下拉框的某个选项
     selectOption(data) {
       if(data) {
@@ -455,26 +496,6 @@ export default {
     },
     cancelAdd() {
       this.dialogVisible = false;
-    },
-    download(data) {//下载证明材料
-      var fileName = data.url.split('/').reverse()[0]
-      var url = data.url
-      axios({
-        url: '/project/basic/downloadByUrl?url=' + url,
-        method: 'GET',
-        responseType: 'blob',
-        headers: {
-          'token': localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).token : ''
-        }
-      }).then(response => {
-        const url = window.URL.createObjectURL(new Blob([response]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', fileName);
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      });
     },
     handleDelete() {//删除选择的文件
       var file={filepath:this.urlFile}
@@ -582,6 +603,14 @@ export default {
       this.title_show = "显示详情";
       this.currentProject = data
       this.dialogVisible_showInfo = true
+      this.isPdf = this.isImage = false; //初始化
+      this.previewUrl = '';
+      this.previewImageSrcList = [];
+      if(data.url.includes('.pdf')) { //判断文件类型
+        this.isPdf = true;
+      } else if(data.url.includes('.jpg') || data.url.includes('.png') || data.url.includes('.jpe')) {
+        this.isImage = true;
+      }
       this.getRequest("/oper/basic/List?prodId=" + data.id + '&type=纵向科研项目').then((resp) => {
         this.loading = false;
         if (resp) {
