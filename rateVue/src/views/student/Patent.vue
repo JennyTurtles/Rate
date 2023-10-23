7014529184f63d3cc46098290e12f8175a91c897
luyiru <luyiru2021@163.com>
2023-10-23 17:18:33 +0800

    1。学生专利修改预览和下载功能

--- rateVue/src/views/student/Patent.vue
+++ rateVue/src/views/student/Patent.vue
@@ -260,11 +260,23 @@
         </el-form-item>
         <el-form-item label="证明材料:" prop="url">
           <span v-if="currentPatent.url == '' || currentPatent.url == null ? true:false" >无证明材料</span>
-          <a v-else style="color:gray;font-size:11px;text-decoration:none;cursor:pointer"
-             @click="download(currentPatent)"
-             onmouseover="this.style.color = 'blue'"
-             onmouseleave="this.style.color = 'gray'">
-            {{currentPatent.url|fileNameFilter}}</a>
+          <div v-else>{{ currentPatent.url | fileNameFilter }}</div>
+          <br />
+          <div v-show="currentPatent.url == '' || currentPatent.url == null ? false : true">
+            <div>
+              <el-button @click="previewMethod('1')" v-show="isImage || isPdf">预览</el-button>
+              <el-button @click="previewMethod('2')">下载</el-button>
+            </div>
+            <div style="margin-top: 5px">
+              <el-image
+                  v-show="false"
+                  ref="previewImage"
+                  style="width: 100px; height: 100px"
+                  :src="previewUrl"
+                  :preview-src-list="previewImageSrcList">
+              </el-image>
+            </div>
+          </div>
           <br />
         </el-form-item>
         <div >
@@ -303,6 +315,14 @@
         ></el-tree>
       </span>
     </el-dialog>
+    <el-dialog :visible.sync="dialogPreviewPdfFile" style="width: 100%;height: 100%">
+      <template v-if="isPdf">
+        <vue-office-pdf
+            :src="previewUrl"
+            style="height: 100vh;"
+        />
+      </template>
+    </el-dialog>
   </div>
 </template>
 
@@ -313,6 +333,11 @@ export default {
   name: "SalSearch",
   data() {
     return {
+      isImage: false,
+      isPdf: false,
+      dialogPreviewPdfFile: false,
+      previewImageSrcList: [],
+      previewUrl: '',
       defaultExpandedKeys: [],
       disabledGrantedStatusSelected: true,
       zeroPointReason: '',
@@ -422,6 +447,23 @@ export default {
     }
   },
   methods: {
+    previewMethod(type) {
+      if(type == '1') {
+        this.previewFileMethod(this.currentPatent).then(res => {
+          this.previewUrl = res;
+          if(this.isImage) {
+            this.previewImageSrcList = [res];
+            this.$refs.previewImage.showViewer = true;
+          }
+          if(this.isPdf) {
+            this.dialogPreviewPdfFile = true;
+          }
+        });
+      } else {
+        this.downloadFileMethod(this.currentPatent);
+      }
+    },
+
     judgeGrantedStatusSelected(data) {
       if(data.level == null || data.level == '') {
         this.patentStatusList = this.patentStatusListObject.slice(3);
@@ -476,23 +518,6 @@ export default {
     //添加 编辑框点击取消出发事件
     cancelAddPatent() {
       this.dialogVisible = false;
-    },
-    download(data){//下载证明材料
-      var fileName = data.url.split('/').reverse()[0]
-      var url = data.url
-      axios({
-        url: '/patent/basic/downloadByUrl?url='+url,
-        method: 'GET',
-        responseType: 'blob'
-      }).then(response => {
-        const url = window.URL.createObjectURL(new Blob([response]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', fileName);
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-      });
     },
     handleDelete() {//删除选择的文件
       var file={
@@ -608,6 +633,14 @@ export default {
       this.title_show = "显示详情";
       this.currentPatent = data
       this.dialogVisible_showInfo = true
+      this.isPdf = this.isImage = false; //初始化
+      this.previewUrl = '';
+      this.previewImageSrcList = [];
+      if(data.url.includes('.pdf')) { //判断文件类型
+        this.isPdf = true;
+      } else if(data.url.includes('.jpg') || data.url.includes('.png') || data.url.includes('.jpe')) {
+        this.isImage = true;
+      }
       this.getRequest("/oper/basic/List?prodId=" + data.id + '&type=授权专利').then((resp) => {
         this.loading = false;
         if (resp) {
