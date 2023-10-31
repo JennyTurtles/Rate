4f30f2f91412eb19ef3c400418263899e892b8ad
luyiru <luyiru2021@163.com>
2023-10-31 17:11:52 +0800

    竞赛微调样式和修改bug

--- rateVue/src/views/student/Academic-Competition.vue
+++ rateVue/src/views/student/Academic-Competition.vue
@@ -250,7 +250,7 @@
     >
       <el-form
           :label-position="labelPosition"
-          label-width="120px"
+          label-width="80px"
           :model="currentCompetition"
           style="margin-left: 20px">
         <el-form-item label="竞赛名称:">
@@ -265,10 +265,6 @@
           <span>{{ currentCompetition.competitionType.name }}</span
           >
         </el-form-item>
-<!--        <el-form-item label="获奖级别:">-->
-<!--          <span>{{ currentCompetition.competitionLevel }}</span-->
-<!--          >-->
-<!--        </el-form-item>-->
         <el-form-item label="获奖年月:">
           <span>{{ currentCompetition.date }}</span
           >
@@ -296,31 +292,27 @@
         <el-form-item label="证明材料:" prop="url">
           <span v-if="currentCompetition.url == '' || currentCompetition.url == null ? true:false" >无证明材料</span>
           <div v-else>{{ currentCompetition.url | fileNameFilter }}</div>
-          <br />
-          <div v-show="currentCompetition.url == '' || currentCompetition.url == null ? false : true">
-            <div>
-              <el-button @click="previewMethod('1')" v-show="isImage || isPdf">预览</el-button>
-              <el-button @click="previewMethod('2')">下载</el-button>
-            </div>
-            <div style="margin-top: 5px">
-              <el-image
-                  v-show="false"
-                  ref="previewImage"
-                  style="width: 100px; height: 100px"
-                  :src="previewUrl"
-                  :preview-src-list="previewImageSrcList">
-              </el-image>
-            </div>
+        </el-form-item>
+        <div v-show="currentCompetition.url == '' || currentCompetition.url == null ? false : true" style="margin-left: 80px">
+          <div>
+            <el-button @click="previewMethod('1')" v-show="isImage || isPdf">预览</el-button>
+            <el-button @click="previewMethod('2')">下载</el-button>
           </div>
-          <br />
-        </el-form-item>
-        <el-form-item label="相关备注:">
-          <span>{{ currentCompetition.remark }}</span>
-        </el-form-item>
+          <div style="margin-top: 5px">
+            <el-image
+                v-show="false"
+                ref="previewImage"
+                style="width: 100px; height: 100px"
+                :src="previewUrl"
+                :preview-src-list="previewImageSrcList">
+            </el-image>
+          </div>
+        </div>
+        <br />
         <div >
           <span>历史操作:</span>
           <div style="margin-top:10px;border:1px solid lightgrey;margin-left:2em;width:400px;height:150px;overflow:scroll">
-            <div  v-for="item in operList" :key="item.time" style="margin-top:18px;color:gray;font-size:5px;margin-left:5px">
+            <div  v-for="item in operList" :key="item.time" style="margin-top:18px;color:gray;margin-left:5px">
               <div >
                 <p>{{item.time | dataFormat}}&nbsp;&nbsp;&nbsp;&nbsp;{{item.operatorName}}&nbsp;&nbsp;&nbsp;&nbsp;{{item.operationName}}</p>
                 <p v-show="item.remark == '' || item.remark == null ? false : true">驳回理由：{{item.remark}}</p>
@@ -647,7 +639,7 @@ export default {
       this.previewImageSrcList = [];
       if(data.url.includes('.pdf')) { //判断文件类型
         this.isPdf = true;
-      } else if(data.url.includes('.jpg') || data.url.includes('.png') || data.url.includes('.jpe')) {
+      } else if(data.url.includes('.jpg') || data.url.includes('.png') || data.url.includes('.jpe') || data.url.includes('.JPG') || data.url.includes('.PNG') || data.url.includes('.JPE')) {
         this.isImage = true;
       }
       this.getRequest("/oper/basic/List?prodId=" + data.id + '&type=学科竞赛').then((resp) => {
