5460384f82431b7e8cbda7423c3955510a175f8e
luyiru <luyiru2021@163.com>
2023-10-31 15:18:31 +0800

    横向修改

--- rateVue/src/views/student/Horizontal-Research-Project.vue
+++ rateVue/src/views/student/Horizontal-Research-Project.vue
@@ -226,23 +226,23 @@
           <span>{{ currentProject.startDate }}</span
           ><br />
         </el-form-item>
-        <el-form-item label="完成人:" prop="author">
+        <el-form-item label="作者列表:" prop="author">
           <span>{{ currentProject.author }}</span
           ><br />
         </el-form-item>
-        <el-form-item label="总人数:" prop="total">
+        <el-form-item label="作者人数:" prop="total">
           <span>{{ currentProject.total }}</span
           ><br />
         </el-form-item>
-        <el-form-item label="排名:" prop="rank">
+        <el-form-item label="作者排名:" prop="rank">
           <span>{{ currentProject.rank }}</span
           ><br />
         </el-form-item>
-        <el-form-item label="积分:" prop="point">
+        <el-form-item label="成果积分:" prop="point">
           <span>{{ currentProject.point }}</span
           ><br />
         </el-form-item>
-        <el-form-item label="状态:" prop="state">
+        <el-form-item label="成果状态:" prop="state">
           <span>{{ currentProject.state=="commit"
               ? "已提交"
               :currentProject.state=="tea_pass"
@@ -257,28 +257,27 @@
         <el-form-item label="证明材料:" prop="url">
           <span v-if="currentProject.url == '' || currentProject.url == null ? true:false" >无证明材料</span>
           <div v-else>{{ currentProject.url | fileNameFilter }}</div>
-          <br />
-          <div v-show="currentProject.url == '' || currentProject.url == null ? false : true">
-            <div>
-              <el-button @click="previewMethod('1')" v-show="isImage || isPdf">预览</el-button>
-              <el-button @click="previewMethod('2')">下载</el-button>
-            </div>
-            <div style="margin-top: 5px">
-              <el-image
-                  v-show="false"
-                  ref="previewImage"
-                  style="width: 100px; height: 100px"
-                  :src="previewUrl"
-                  :preview-src-list="previewImageSrcList">
-              </el-image>
-            </div>
-          </div>
-          <br />
         </el-form-item>
+        <div v-show="currentProject.url == '' || currentProject.url == null ? false : true" style="margin-left: 80px">
+          <div>
+            <el-button @click="previewMethod('1')" v-show="isImage || isPdf">预览</el-button>
+            <el-button @click="previewMethod('2')">下载</el-button>
+          </div>
+          <div style="margin-top: 5px">
+            <el-image
+                v-show="false"
+                ref="previewImage"
+                style="width: 100px; height: 100px"
+                :src="previewUrl"
+                :preview-src-list="previewImageSrcList">
+            </el-image>
+          </div>
+        </div>
+        <br />
         <div >
           <span>历史操作:</span>
           <div style="margin-top:10px;border:1px solid lightgrey;margin-left:2em;width:400px;height:150px;overflow:scroll">
-            <div  v-for="item in operList" :key="item.time" style="margin-top:18px;color:gray;font-size:5px;margin-left:5px">
+            <div  v-for="item in operList" :key="item.time" style="margin-top:18px;color:gray;margin-left:5px">
               <div >
                 <p>{{item.time | dataFormat}}&nbsp;&nbsp;&nbsp;&nbsp;{{item.operatorName}}&nbsp;&nbsp;&nbsp;&nbsp;{{item.operationName}}</p>
                 <p v-show="item.remark == '' || item.remark == null ? false : true">驳回理由：{{item.remark}}</p>
@@ -575,7 +574,7 @@ export default {
       this.previewImageSrcList = [];
       if(data.url.includes('.pdf')) { //判断文件类型
         this.isPdf = true;
-      } else if(data.url.includes('.jpg') || data.url.includes('.png') || data.url.includes('.jpe')) {
+      } else if(data.url.includes('.jpg') || data.url.includes('.png') || data.url.includes('.jpe') || data.url.includes('.JPG') || data.url.includes('.PNG') || data.url.includes('.JPE')) {
         this.isImage = true;
       }
       this.getRequest("/oper/basic/List?prodId=" + data.id + '&type=横向科研项目').then((resp) => {
