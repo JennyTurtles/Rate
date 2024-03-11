1c6c358422482331f006b78eb73415f9fe973dd9
wyy <1344337528@qq.com>
2024-03-11 16:24:28 +0800

    https配置

--- rate/rateserver/rate-web/src/main/java/org/sys/rate/RateApplication.java
+++ rate/rateserver/rate-web/src/main/java/org/sys/rate/RateApplication.java
@@ -1,8 +1,13 @@
 package org.sys.rate;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.cache.annotation.EnableCaching;
@@ -33,5 +38,44 @@ public class RateApplication {
         };
     }
 
+//application.properties配置：
+// === tomcat (https)START ===
+//  server.port= 443
+//  server.ssl.key-store=classpath:dhu.jks
+//  server.ssl.key-store-password=DhuXxb@06272023
+//  server.ssl.key-store-type=JKS
+//  server.ssl.enabled-protocols=TLSv1,TLSv1.1,TLSv1.2
+//  server.ssl.ciphers=TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256,TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA384,TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,TLS_ECDHE_RSA_WITH_RC4_128_SHA,TLS_RSA_WITH_AES_128_CBC_SHA256,TLS_RSA_WITH_AES_128_CBC_SHA,TLS_RSA_WITH_AES_256_CBC_SHA256,TLS_RSA_WITH_AES_256_CBC_SHA,SSL_RSA_WITH_RC4_128_SHA
+// === tomcat (https)END ===
 
+    /* === 同时拥有http并重定向至https端口上: START === */
+     @Bean
+     public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+         TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+             @Override
+             protected void postProcessContext(Context context) {
+                 SecurityConstraint securityConstraint = new SecurityConstraint();
+                 securityConstraint.setUserConstraint("CONFIDENTIAL");
+                 SecurityCollection securityCollection = new SecurityCollection();
+                 securityCollection.addPattern("/*");
+                 securityConstraint.addCollection(securityCollection);
+                 context.addConstraint(securityConstraint);
+             }
+         };
+         factory.addAdditionalTomcatConnectors(httpConnector());
+         return factory;
+     }
+
+     @Bean
+     public Connector httpConnector() {
+         Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+         connector.setScheme("http");
+         //Connector监听的http的端口号
+         connector.setPort(80);
+         connector.setSecure(false);
+         //监听到http的端口号后转向到的https的端口号
+         connector.setRedirectPort(443);
+         return connector;
+     }
+    /* === 同时拥有http并重定向至https端口上: END === */
 }
