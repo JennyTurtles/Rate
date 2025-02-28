d2c3f4214a2d9157f8e09f9914117b71398b939a
ybc <1796662425@qq.com>
2025-02-28 19:15:36 +0800

    112

---
+++ rate/rateserver/rate-model/src/main/java/org/sys/rate/model/XinProjectVo.java
@@ -0,0 +1,13 @@
+package org.sys.rate.model;
+
+import lombok.Data;
+
+@Data
+public class XinProjectVo {
+
+ private String   pointBack;
+ private String   name;
+ private String   pointFront;
+ private String   state;
+ private String   studentName;
+}
